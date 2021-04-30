e6ba021d6cd442f25dbd296af1ff0df22785cdab
vivek singh <vivek.vns.056@gmail.com>
2021-04-29 22:24:01 -0400

    first commit for base line code base

---
+++ src/components/blog-dashboard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Blog = (props) => {
+    return(
+        <div>this is blog dashboard</div>
+    )
+}
+
+
+export default Blog;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Blog  from './components/blog-dashboard';  
+import SignUp from './components/SignUp';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
